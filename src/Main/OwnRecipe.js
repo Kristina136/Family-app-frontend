33882a79aecf916c1480916f3e6b57cbc4774ae6
Kristina <111386621+Kristina136@users.noreply.github.com>
2023-09-27 08:55:01 +0200

    Add files via upload

--- src/Main/OwnRecipe.js
+++ src/Main/OwnRecipe.js
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
-import { addRecipe, getAllRecipes, deleteRecipe, editRecipe } from "../FetchApp";
+import {
+  addRecipe,
+  getAllRecipes,
+  deleteRecipe,
+  editRecipe,
+} from "../FetchApp";
 import { MyRecipe } from "./MyRecipe";
 import { SiReacthookform } from "react-icons/si/";
 import { FiChevronsLeft } from "react-icons/fi/";
@@ -18,10 +23,11 @@ export const OwnRecipe = ({
   const [editing, setEditing] = useState(false);
   const [recipeID, setRecipeId] = useState("");
   const [openForm, setOpenForm] = useState(false);
-
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getAllRecipes(setMyRecipe);
+    setLoading(false);
   }, [setMyRecipe]);
 
   const updatingInInput = (_id, title, setTitle, description, image) => {
@@ -35,41 +41,56 @@ export const OwnRecipe = ({
 
   return (
     <div className="marginLeft">
-        <button className="openFormBtn" onClick={() => setOpenForm(!openForm)}>
-         <SiReacthookform/> Open Form to create your recipe <FiChevronsLeft className={ openForm ? "transformUp" : "transformDown"}  size={30} color="#69696b"/>
-        </button>
-     
-     <Form openForm={openForm}
-     title={title} 
-     setTitle={setTitle}
-      description={description}
-     setDescription={setDescription}
-      setImage={setImage}
-      editing={editing}
-      recipeID={recipeID}
-      editRecipe={editRecipe}
-      image={image}
-      setMyRecipe={setMyRecipe}
-      setEditing={setEditing}
-       addRecipe={addRecipe}
-       setOpenForm={setOpenForm}
-     />
-<h1>Your favorite family recipes</h1>
-      <div className="recipeContainer">
-        {myRecipe.map((recipe) => (
-          <MyRecipe
-            text={recipe.title}
-            description={recipe.description}
-            image={recipe.image}
-            key={recipe._id}
-            _id={recipe._id}
-            setMyRecipe={setMyRecipe}
-            deleteRecipe={deleteRecipe}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {" "}
+          <button
+            className="openFormBtn"
+            onClick={() => setOpenForm(!openForm)}
+          >
+            <SiReacthookform /> Open Form to create your recipe{" "}
+            <FiChevronsLeft
+              className={openForm ? "transformUp" : "transformDown"}
+              size={30}
+              color="#69696b"
+            />
+          </button>
+          <Form
+            openForm={openForm}
+            title={title}
             setTitle={setTitle}
-            updatingInInput={updatingInInput}
+            description={description}
+            setDescription={setDescription}
+            setImage={setImage}
+            editing={editing}
+            recipeID={recipeID}
+            editRecipe={editRecipe}
+            image={image}
+            setMyRecipe={setMyRecipe}
+            setEditing={setEditing}
+            addRecipe={addRecipe}
+            setOpenForm={setOpenForm}
           />
-        ))}
-      </div>
+          <h1>Your favorite family recipes</h1>
+          <div className="recipeContainer">
+            {myRecipe.map((recipe) => (
+              <MyRecipe
+                text={recipe.title}
+                description={recipe.description}
+                image={recipe.image}
+                key={recipe._id}
+                _id={recipe._id}
+                setMyRecipe={setMyRecipe}
+                deleteRecipe={deleteRecipe}
+                setTitle={setTitle}
+                updatingInInput={updatingInInput}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
